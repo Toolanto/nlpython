eccacae3cce43478576b01aad5859212c02ea14f
Toolanto <antonio@antonio.(none)>
2013-06-08 19:21:02 +0200

    search ngram

--- nlptool.py
+++ nlptool.py
@@ -4,6 +4,7 @@
 import nltk
 import itertools
 import math
+import re
 from documentHandler import *
 from store import *
 
@@ -30,49 +31,55 @@ class NLP():
     self.corpus = {key : [word for word in self.corpus[key] if word not in listWord] for key in self.corpus.keys()} 
   
   def getDocumentsByToken(self,token):
-    docsID = [key for key in self.corpus.keys() if token in self.corpus[key]] #tutti i documenti che hanno il token
+    docsID = [key for key in self.corpus.keys() if self.calculate_tf(token,self.corpus[key])>0] #tutti i documenti che hanno il token
     return [(idn,self.corpusDB.searchByIdentifier(idn)) for idn in docsID] #prelevo dal db i documenti senza modifiche
   
-  #ngramm è di tipo Ngram
-  def getDocumentsByGramm(self,gramm,ngramm):
-    self.corpusngram = {key : [ngram.findgram(value)] for key,value in self.corpus.iteritems()} 
-    docsID = [key for key in self.corpusngram.keys() if token in self.corpusngram[key]] #tutti i documenti che hanno l'ngram
-    return [(idn,self.corpusDB.searchByIdentifier(idn)) for idn in docsID] #prelevo dal db i documenti senza modifiche  
+  #calcola il numero di occorenze di un termine in un documento, il termine può essere composto
+  def calculate_tf(self,term,text):
+    #un token può essere anche un ngramma perciò ogni documento viene convertito in stringa e si fa
+    #il match per vedere quante volte compare il token all'interno del documento
+    document_str = ' '.join(text)
+    pattern = r"(^{0}| {0})".format(term) #termine che puo trovarsi all'inizio del documento o al centro ma non può
+                                          #essere una parte di un altro termine
+    return len(re.findall(pattern,document_str))   
+   
+  #def getDocumentsByToken_token(self,token):
+  #  docsID = [key for key in self.corpus.keys() if token in self.corpus[key]] #tutti i documenti che hanno il token
+  #  return [(idn,self.corpusDB.searchByIdentifier(idn)) for idn in docsID] #prelevo dal db i documenti senza modifiche
 
-  #document è la lista di token di un documento
-  def calculate_tf(self,token,document):
-    return document.count(token)
+  #def calculate_tf_token(self,token,document):
+  #  return document.count(token)
   
   def calculate_idf(self,token):
     return math.log(float(len(self.corpus))/float((len(self.getDocumentsByToken(token))+1)))
   
   def calculate_tfidf(self,token,document):
+    print self.calculate_tf(token,document)
+    print self.calculate_idf(token)
     return self.calculate_tf(token,document)*self.calculate_idf(token)
   
-  def findmngram(self,ngram):
-    return {key:ngram.findgram(doc) for key,doc in self.corpus.items()}
-      
+  def findngram(self,ngram):
+    return {key:nltk.util.ngrams(doc,ngram) for key,doc in self.corpus.items()}
+  
         
-
-
-
+'''
 class Ngram():
-  def findgram(self,text):
+  def findgram(self,text,number):
     pass
 
 class Bigram(Ngram):
   #text = ['a',.]
   def findgram(self,text):
-    bigram_measures = nltk.BigramAssocMeasures()
-    bigrams = nltk.BigramCollocationFinder.from_words(text)
-    return bigrams.nbest(bigram_measures.pmi,10)
-    #return nltk.util.ngrams(text,self._ngram)
+    #bigram_measures = nltk.BigramAssocMeasures()
+    #bigrams = nltk.BigramCollocationFinder.from_words(text)
+    #return bigrams.nbest(bigram_measures.pmi,10)
+    return nltk.util.ngrams(text,2)
 
 class Trigramm(Ngram):
   #text = ['a',.]
   def findgram(self,text):
-    return nltk.util.ngrams(text,self._ngram)
-             
+    return nltk.util.ngrams(text,3)
+'''             
     
 #Passando una regex  al costruttore si ha una tokenizzazione custom
 class CustomTokenizer(nltk.RegexpTokenizer):
@@ -88,7 +95,9 @@ if __name__=='__main__':
   print nlp.getCorpus()
   #nlp.removeStop(['casa','ciao'])
   print nlp.getCorpus()     
-  print nlp.getDocumentsByToken("sono")
-  print "tf: {0}, idf: {1}, tf-idf di sono nel documento 0: {2}".format(nlp.calculate_tf("sono",nlp.getCorpus()[0]),nlp.calculate_idf("sono"),nlp.calculate_tfidf("sono",nlp.getCorpus()[0]))
-  print nlp.transformngram(Bigram())
+  print nlp.getDocumentsByToken("sono a casa")
+  print "tf: {0}".format(nlp.calculate_tf("sono a",nlp.getCorpus()[0]))
+  print "idf: {0}".format(nlp.calculate_idf("sono a")) 
+  print "tf_idf: {0}".format(nlp.calculate_tfidf("sono a",nlp.getCorpus()[0]))
+  #print nlp.findngram(3)
   
