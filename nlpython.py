81eb6c9584f216d17bf6c0e58a7ae5a478023b71
Toolanto <antonio@antonio.(none)>
2013-06-08 22:47:23 +0200

    find ngram

--- nlpython.py
+++ nlpython.py
@@ -3,7 +3,6 @@
 
 from nlptool import * 
   
-
 print "\n\n******* NLP programm *******\n"
 documents = raw_input ("Give corpus's path: ")
 nlp = NLP(documents,"corpus.db")
@@ -27,21 +26,24 @@ while (exit):
            Reply:  '''))      
   if n == 1:
     term = raw_input("\nEnter term: ")
-    print "\tidDoc\ttf"
+    print "\tidDoc\t\ttf"
     for key in nlp.getCorpus().keys():
       tf = nlp.calculate_tf(term,nlp.getCorpus()[key])
-      print "\t{0}\t{1}".format(key,tf)
+      print "\t{0}\t\t{1}".format(key,tf)
   if n == 2:
     token = raw_input("\nEnter token: ")
     documents = nlp.getDocumentsByToken(token)
     print documents
   if n == 3:
     document = input("\nEnter document's id: ")
-    print "\tterm\ttf-idf"
+    print "\tterm\t\ttf-idf"
     for term in nlp.getCorpus()[document]:
       tfidf = nlp.calculate_tfidf(term,nlp.getCorpus()[document])
-      print "\t{0}\t{1}".format(term,tfidf)
-  if n not in[1,2,3]:
+      print "\t{0}\t\t{1}".format(term,tfidf)
+  if n == 4:
+    document = input("\nEnter deocument's id: ")
+    print nlp.findngram(document,2,0.3)
+  if n not in[1,2,3,4]:
     exit = False
+nlp.delete()
 
-#print nlp.transformngram(Bigram())
